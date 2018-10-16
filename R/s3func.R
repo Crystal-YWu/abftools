91ccabf92fdb29e811a2a8249470f77d1d015295
Jun <imlijunda@gmail.com>
2018-10-16 23:34:11 +1030

    Use 0-based channel numbering for [[.

--- R/s3func.R
+++ R/s3func.R
@@ -14,18 +14,22 @@ print.abf <- function(x, ...) {
 `[[.abf` <- function(x, channel) {
 
   d <- dim(x)
-  if (length(channel) > 1)
+  if (length(channel) > 1L)
     stop("Extract channel: you can only select one channel.")
-  if (channel > d[1])
+  if (channel > d[1L])
     stop(paste0("Extract channel: channel ", channel, " not available."))
+
+  #when using [[ to extract, we use 0 based index so the channel matches that was
+  #set in pClamp software
+  channel <- channel + 1L
 
   epi <- GetAvailEpisodes(x)
   #in case of only one episode
-  if (length(epi) == 1) {
+  if (length(epi) == 1L) {
     #1d vector
     df <- x[channel, ,epi]
     #2d matrix
-    df <- matrix(df, ncol = 1)
+    df <- matrix(df, ncol = 1L)
   } else {
     #2d matrix
     df <- x[channel, , epi]
@@ -44,7 +48,7 @@ print.abf <- function(x, ...) {
 #' @export
 #'
 #' @examples
-as.data.frame.abf <- function(x, channel = 1) {
+as.data.frame.abf <- function(x, channel = 0L) {
 
   df <- x[[channel]]
 
