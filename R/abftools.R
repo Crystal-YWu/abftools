0c950b493da7b43e061ed0c85109df1e43be67c3
Jun <imlijunda@gmail.com>
2018-11-14 05:02:40 +1030

    Only import used methods to avoid library warnings.

--- R/abftools.R
+++ R/abftools.R
@@ -1,8 +1,8 @@
 ##' @import ggplot2
 ##' @import reshape2
 ##' @importFrom cowplot plot_grid
-##' @import matrixStats
-##' @import stats
-##' @import Matrix
+##' @importFrom matrixStats colSds
+##' @importFrom stats sd
+##' @importFrom Matrix Diagonal sparseMatrix solve
 ##' @import wmtsa
 NULL
