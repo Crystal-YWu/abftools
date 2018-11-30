f365450df8d3bbd3c8149d2c86a64f178812e821
Jun <imlijunda@gmail.com>
2018-11-30 11:22:08 +1030

    Plot markers on QuickPlot_IVSummary.

--- R/QuickPlot.R
+++ R/QuickPlot.R
@@ -4,14 +4,16 @@
 #' @param pos an interval/cursor or a list of intervals/cursors.
 #' @param colour whether to plot in coloured mode.
 #' @param title OPTIONAL, title for the plot.
+#' @param legend_title OPTIONAL, title of the legend.
 #' @param zero_intercept whether to add zero intercepts to the plot.
 #' @param zero_axes whether to plot axes at 0.
+#' @param line_size size of lines
 #'
 #' @return a ggplot object.
 #' @export
 #'
 QuickPlotIV <- function(abf, pos, colour = FALSE, title = NULL, legend_title = NULL,
-                        zero_intercept = TRUE, zero_axes = TRUE) {
+                        zero_intercept = TRUE, zero_axes = TRUE, line_size = 0.5) {
 
   if (!IsAbf(abf) && !IsAbfList(abf)) {
     err_class_abf()
@@ -35,15 +37,14 @@ QuickPlotIV <- function(abf, pos, colour = FALSE, title = NULL, legend_title = N
   ycol <- as.name(cname[3L])
   p <- ggplot(melted, aes_string(x = xcol, y = ycol)) + theme_classic()
   if (colour) {
-    p <- p + geom_line(aes_string(colour = "id"))
+    p <- p + geom_line(aes_string(colour = "id"), size = line_size)
     if (is.null(legend_title)) {
       p <- p + theme(legend.title = element_blank())
     } else {
       p <- p + labs(`colour` = as.character(legend_title))
     }
-
   } else {
-    p <- p + geom_line(aes_string(group = "id"))
+    p <- p + geom_line(aes_string(group = "id"), size = line_size)
   }
 
   #Add zero intercepts
@@ -99,40 +100,49 @@ QuickPlotTrace <- function(abf, channelX, episodeX, channelY, episodeY, intv) {
 
   qplot(x = traceX, y = traceY, geom = "point") + theme_bw()
 }
+
 #' Quick plot an IV Summary table
 #'
 #' @param df_summary an IV summary table from IVSummary()
-#' @param err_bar_width width of the error bar
 #' @param title OPTIONAL, title of the plot
 #' @param legend_title OPTIONAL, if multiple IV summaries are to plot, title of the legend.
 #' @param zero_intercept whether to add zero intercepts to the plot.
 #' @param zero_axes whether to plot axes at 0.
+#' @param line_size size of lines
+#' @param marker_size size of markers
+#' @param err_bar_size size of error bar
+#' @param err_bar_width width of error bar
 #'
 #' @return a ggplot object.
 #' @export
 #'
-QuickPlot_IVSummary <- function(df_summary, err_bar_width = 1.5, title = NULL, legend_title = NULL,
-                                zero_intercept = TRUE, zero_axes = TRUE) {
+QuickPlot_IVSummary <- function(df_summary, title = NULL, legend_title = NULL,
+                                zero_intercept = TRUE, zero_axes = TRUE,
+                                line_size = 0.5, marker_size = line_size * 4,
+                                err_bar_size = line_size / 1.5,
+                                err_bar_width = marker_size * 1.5) {
 
   if (class(df_summary) == "data.frame") {
 
-    colnames(df_summary) <- c("Voltage", "SEMVoltage", "Current", "SEMCurrent")
-    p <- ggplot(data = df_summary, mapping = aes(x = Voltage, y = Current, group = 1))
-    p <- p + geom_line()
-    p <- p + geom_errorbar(mapping = aes(ymin = Current - SEMCurrent, ymax = Current + SEMCurrent),
-                           width = err_bar_width)
-    p <- p + geom_point()
-    if (zero_intercept) {
+    colnames(df_summary) <- c("Voltage", "SEMV", "Current", "SEMC")
+    p <- ggplot(data = df_summary, mapping = aes(x = Voltage, y = Current))
+    p <- p + geom_line(size = line_size)
+
+    p <- p + geom_errorbar(mapping = aes(ymin = Current - SEMC, ymax = Current + SEMC),
+                           size = err_bar_size, width = err_bar_width)
+
+    p <- p + geom_point(size = marker_size)
+
+    if (!zero_axes && zero_intercept) {
       p <- p + geom_vline(xintercept = 0, linetype = "dashed") +
         geom_hline(yintercept = 0, linetype = "dashed")
     }
-
-    p <- p + theme_classic()
 
     if (!is.null(title)) {
       p <- p + ggtitle(title)
     }
 
+    p <- p + theme_classic()
     if (zero_axes) {
       p <- p + ZeroAxes(xlimit = df_summary[, 1], ylimit = df_summary[, 3],
                         xlabel = "Voltage", ylabel = "Current")
@@ -149,17 +159,19 @@ QuickPlot_IVSummary <- function(df_summary, err_bar_width = 1.5, title = NULL, l
     }
     colnames(df_melted) <- c("id", "Voltage", "SEMV", "Current", "SEMC")
 
-    p <- ggplot(data = df_melted, mapping = aes_string(x = "Voltage", y = "Current", color = "id"))
-    p <- p + geom_line()
+    p <- ggplot(data = df_melted, mapping = aes(x = Voltage, y = Current, color = id))
+    p <- p + geom_line(size = line_size)
+
     p <- p + geom_errorbar(mapping = aes(ymin = Current - SEMC, ymax = Current + SEMC),
-                           width = err_bar_width)
-    p <- p + geom_point()
+                           size = err_bar_size, width = err_bar_width)
+
+    p <- p + geom_point(mapping = aes(shape = id), size = marker_size)
+
     if (!zero_axes && zero_intercept) {
       p <- p + geom_vline(xintercept = 0, linetype = "dashed") +
         geom_hline(yintercept = 0, linetype = "dashed")
     }
 
-    p <- p + theme_classic()
     if (is.null(legend_title)) {
       p <- p + theme(legend.title = element_blank())
     } else {
@@ -170,6 +182,7 @@ QuickPlot_IVSummary <- function(df_summary, err_bar_width = 1.5, title = NULL, l
       p <- p + ggtitle(title)
     }
 
+    p <- p + theme_classic()
     if (zero_axes) {
       p <- p + ZeroAxes(xlimit = df_melted[, 2], ylimit = df_melted[, 4],
                         xlabel = "Voltage", ylabel = "Current")
