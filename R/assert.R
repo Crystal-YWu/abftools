21d8cfe59e5fc19bbf33290d95530c6de32fcf86
Jun <imlijunda@gmail.com>
2019-02-12 01:03:31 +1030

    Assert for list and CheckArgs.

--- R/assert.R
+++ R/assert.R
@@ -8,18 +8,18 @@ IsAbfList <- function(x) {
 IsListOf <- function(x, cls) {
 
   if (!is.list(x)) {
-    return(FALSE)
+    FALSE
   } else {
-    return(all(sapply(x, function(item) all(cls %in% class(item)))))
+    all(sapply(x, function(item) all(cls %in% class(item))))
   }
 }
 
 IsListContains <- function(x, cls) {
 
   if (!is.list(x)) {
-    return(FALSE)
+    FALSE
   } else {
-    return(all(sapply(x, function(item) any(cls %in% class(item)))))
+    all(sapply(x, function(item) any(cls %in% class(item))))
   }
 }
 
@@ -42,15 +42,78 @@ AssertLength <- function(x, ..., explicit = NULL) {
 
 AssertEpisode <- function(abf, episode) {
 
-  all(nEpi(abf) >= episode & episode > 0L)
+  f <- function(abf, episode) {
+    length(episode) && all(nEpi(abf) >= episode & episode > 0L)
+  }
+
+  if (IsAbf(abf)) {
+    f(abf, episode)
+  } else {
+    all(sapply(abf, f, episode = episode))
+  }
 }
 
 AssertChannel <- function(abf, channel) {
 
-  all(nChan(abf) >= channel & channel > 0L)
+  f <- function(abf, channel) {
+    length(channel) && all(nChan(abf) >= channel & channel > 0L)
+  }
+
+  if (IsAbf(abf)) {
+    f(abf, channel)
+  } else {
+    all(sapply(abf, f, channel = channel))
+  }
 }
 
 AssertEpoch <- function(abf, epoch) {
 
-  all(nEpoch(abf) >= epoch & epoch > 0L)
+  f <- function(abf, epoch) {
+    length(epoch) && all(nEpoch(abf) >= epoch & epoch > 0L)
+  }
+
+  if (IsAbf(abf)) {
+    f(abf, epoch)
+  } else {
+    all(sapply(abf, f, epoch = epoch))
+  }
+}
+
+AssertDac <- function(abf, dac) {
+
+  f <- function(abf, dac) {
+    nDACNum <- get_meta(abf)$EpochPerDAC$nDACNum
+    length(dac) && all((dac - 1L) %in% nDACNum)
+  }
+
+  if (IsAbf(abf)) {
+    f(abf, dac)
+  } else {
+    all(sapply(abf, f, dac = dac))
+  }
+}
+
+CheckArgs <- function(abf, epi = NULL, chan = NULL, epo = NULL, dac = NULL,
+                      ..., allow_list = FALSE) {
+
+  isabf <- IsAbf(abf)
+  isabflist <- IsAbfList(abf)
+  if (!(isabf || (allow_list && isabflist))) {
+    err_class_abf()
+  }
+
+  if (!is.null(chan) && !AssertChannel(abf, chan)) {
+    err_channel()
+  }
+  if (!is.null(epi) && !AssertEpisode(abf, epi)) {
+    err_episode()
+  }
+  if (!is.null(epo) && !AssertEpoch(abf, epo)) {
+    err_epoch()
+  }
+  if (!is.null(dac) && !AssertDac(abf, dac)) {
+    err_epoch_dac()
+  }
+
+  TRUE
 }
