819ad1c5029f2ab5dd89325b618ab7f418907328
Jun <imlijunda@gmail.com>
2019-02-12 02:40:46 +1030

    Removed obscure MultiIntervalXXX functions.

--- R/episodic.R
+++ R/episodic.R
@@ -1,53 +1,29 @@
-#' MultiIntervalMeans calculates interval means of a channel in a list of abf objects
-#'
-#' @param abf_list a list of abf objects.
-#' @param intv_list a list of intervals.
-#' @param channel channel to calculated, channel id is 1-based.
-#' @param na.rm remove NAs.
-#'
-#' @return a vector of calculated values.
-#' @export
-#'
-MultiIntervalMeans <- function(abf_list, intv_list, channel = 1L, na.rm = TRUE) {
+#This function applies episodic values in given by the intv_list to function f
+#and returns a matrix of the results of f. f should accept na.rm
+Episodic_ColFunc <- function(abf_list, intv_list, channel, f, ...) {
 
-  f <- function(x) colMeans(x, na.rm)
-  mx <- EpisodicInterval_f(abf_list, intv_list, channel, f)
+  n <- length(abf_list)
+  nepi <- max(unlist(nEpi(abf_list)))
 
-  return(mx)
+  m <- matrix(NA, nrow = n, ncol = nepi)
+  colnames(m) <- DefaultEpiLabel(nepi)
+  rownames(m) <- unlist(GetTitle(abf_list))
+
+  for (i in seq_along(abf_list)) {
+    if (any(is.na(intv_list[[i]]))) {
+      next
+    } else {
+      if (is.null(intv_list[[i]])) {
+        mask <- seq_len(nPts(abf_list[[i]]))
+      } else {
+        mask <- MaskIntv(intv_list[[i]])
+      }
+      ret <- f(abf_list[[i]][mask, , channel], ...)
+      epi <- GetAvailEpisodes(abf_list[[i]])
+      for (j in epi) m[i, j] <- ret[j]
+    }
+  }
+
+  m
 }
 
-#' MultiIntervalSds calculates interval standard deviations of a channel in a list of abf objects
-#'
-#' @param abf_list a list of abf objects.
-#' @param intv_list a list of intervals.
-#' @param channel channel to calculated, channel id is 1-based.
-#' @param na.rm remove NAs.
-#'
-#' @return a vector of calculated values.
-#' @export
-#'
-MultiIntervalSds <- function(abf_list, intv_list, channel = 1L, na.rm = TRUE) {
-
-  f <- function(x) matrixStats::colSds(x, na.rm)
-  mx <- EpisodicInterval_f(abf_list, intv_list, channel, f)
-
-  return(mx)
-}
-
-#' MultiIntervalSems calculates interval SEM of a channel in a list of abf objects
-#'
-#' @param abf_list a list of abf objects.
-#' @param intv_list a list of intervals.
-#' @param channel channel to calculated, channel id is 1-based.
-#' @param na.rm remove NAs.
-#'
-#' @return a vector of calculated values.
-#' @export
-#'
-MultiIntervalSems <- function(abf_list, intv_list, channel = 1L, na.rm = TRUE) {
-
-  f <- function(x) colSems(x, na.rm)
-  mx <- EpisodicInterval_f(abf_list, intv_list, channel, f)
-
-  return(mx)
-}
