604811498610f74e8238e39591336b5f0151111d
Jun <imlijunda@gmail.com>
2019-02-22 06:15:28 +1030

    Updated docs.

--- R/abftools.R
+++ R/abftools.R
@@ -1,6 +1,6 @@
 ##' @import ggplot2
 ##' @importFrom cowplot plot_grid
-##' @importFrom stats sd
+##' @importFrom stats sd median
 ##' @importFrom matrixStats colSds colMads
 ##' @importFrom Matrix Diagonal sparseMatrix solve diff
 ##' @importFrom splines bs
