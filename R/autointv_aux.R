f760cc927efd57531817010cac137dcb4f71c88f
Jun <imlijunda@gmail.com>
2019-02-26 15:00:08 +1030

    Apply perturbation to BinSearch

--- R/autointv_aux.R
+++ R/autointv_aux.R
@@ -23,12 +23,30 @@ OverlapEpisodicIntv <- function(episodic_intv, npts) {
   LogiToIntv(ret)
 }
 
-BinSearchIntv <- function(channel_data, intv, min_intv) {
+BinSearchIntv <- function(channel_data, intv, min_intv,
+                          idx_min = NULL, idx_max = NULL) {
 
-  mask <- MaskIntv(intv)
-  ret <- list()
-  ret$score <- mean(channel_data[mask])
-  ret$intv <- intv
+  if (is.null(idx_min)) {
+    idx_min <- intv[1]
+  }
+  if (is.null(idx_max)) {
+    idx_max <- intv[2]
+  }
+
+  #perturbation 20% left/right
+  p_len <- intv[3] %/% 5L
+  intv_l <- ShftIntv(intv, -p_len, idx_min = idx_min, idx_max = idx_max)
+  intv_r <- ShftIntv(intv, p_len, idx_min = idx_min, idx_max = idx_max)
+  score_l <- mean(channel_data[MaskIntv(intv_l)])
+  score_r <- mean(channel_data[MaskIntv(intv_r)])
+  score <- mean(channel_data[MaskIntv(intv)])
+
+  ret <- switch(which.min(c(score,
+                            score_l,
+                            score_r)),
+                list(score = score, intv = intv),
+                list(score = score_l, intv = intv_l),
+                list(score = score_r, intv = intv_r))
 
   if (intv[3] %/% 2L <= min_intv) {
     return(ret)
@@ -38,8 +56,8 @@ BinSearchIntv <- function(channel_data, intv, min_intv) {
   intv_l <- Intv(intv[1], mid_idx)
   intv_r <- Intv(mid_idx, intv[2])
 
-  ret_r <- BinSearchIntv(channel_data, intv_r, min_intv)
-  ret_l <- BinSearchIntv(channel_data, intv_l, min_intv)
+  ret_r <- BinSearchIntv(channel_data, intv_r, min_intv, idx_min, idx_max)
+  ret_l <- BinSearchIntv(channel_data, intv_l, min_intv, idx_min, idx_max)
 
   switch(which.min(c(ret$score,
                      ret_l$score,
